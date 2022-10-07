5316b84b37de241bc3289c77d0328b32ac196369
LukaszHolowacz <lukasz.holowacz5@gmail.com>
2022-10-07 08:31:59 +0200

    działa program (do zrobienia efekty wizualne bo wyglada zle)

--- Hej.py
+++ Hej.py
@@ -1,5 +1,6 @@
+import random
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, END
 
 male_litery = "abcdefghijklmnoqprstuwxyz"
 wielkie_litery = "ABCDEFGHIJKLMNOQPRSTUWXYZ"
@@ -8,20 +9,25 @@ znaki = "!@#$%^&*()_+-={}[]:;\|<>?,./"
 
 def wypisz():
     wynik = ""
-    if malestatus.get()==1:
+    if malestatus.get() == 1:
         wynik += male_litery
-    elif wielkiestatus.get()==1:
+    if wielkiestatus.get() == 1:
         wynik += wielkie_litery
-    elif liczbystatus.get()==1:
+    if liczbystatus.get() == 1:
         wynik += liczby
-    elif znakistatus.get()==1:
+    if znakistatus.get() == 1:
         wynik += znaki
-    else:
+    if malestatus.get() == 0 and wielkiestatus.get() == 0 and liczbystatus.get() == 0 and znakistatus.get() == 0:
         tk.messagebox.showinfo(title="Błąd", message="Musisz coś zaznaczyć")
+
+    result_str = ''.join((random.choice(wynik) for i in range(int(znakitb.get()))))
+
+    entry.config(state="normal")
+    entry.delete(0, END)
+    entry.insert(0, str(result_str))
 
 window = tk.Tk()
 window.geometry("300x200")
-
 
 malestatus = tk.IntVar()
 wielkiestatus = tk.IntVar()
@@ -31,13 +37,13 @@ znakistatus = tk.IntVar()
 button = tk.Button(text="Generuj Hasło", command = wypisz)
 label1 = tk.Label(text="Twoje hasło!")
 entry = tk.Entry(state='disabled')
+ile_znakow = tk.Label(text="Ile znaków ma mieć twoje hasło?")
 znakitb = tk.Entry()
-znakitb.insert(0, 'Ile znaków?')
 
-male = tk.Checkbutton(window, text='Małe Litery',variable=malestatus, onvalue=1, offvalue=0)
-wielkie = tk.Checkbutton(window, text='Wielkie Litery',variable=wielkiestatus, onvalue=1, offvalue=0)
-liczbycb = tk.Checkbutton(window, text='Liczby',variable=liczbystatus, onvalue=1, offvalue=0)
-znakicb = tk.Checkbutton(window, text='Znaki specjalne',variable=znakistatus, onvalue=1, offvalue=0)
+male = tk.Checkbutton(window, text='Małe Litery', variable=malestatus, onvalue=1, offvalue=0)
+wielkie = tk.Checkbutton(window, text='Wielkie Litery', variable=wielkiestatus, onvalue=1, offvalue=0)
+liczbycb = tk.Checkbutton(window, text='Liczby', variable=liczbystatus, onvalue=1, offvalue=0)
+znakicb = tk.Checkbutton(window, text='Znaki specjalne', variable=znakistatus, onvalue=1, offvalue=0)
 
 label1.pack()
 entry.pack()
@@ -45,6 +51,7 @@ male.pack()
 wielkie.pack()
 liczbycb.pack()
 znakicb.pack()
+ile_znakow.pack()
 znakitb.pack()
 button.pack()
 
